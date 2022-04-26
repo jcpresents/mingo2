ef9ce578668e9001d523695fce3639751ac1a84f
jcpresents <43340068+jcpresents@users.noreply.github.com>
2022-04-26 02:29:09 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,229 @@
+// start row 1 left to right
+function showDescription1() {
+  let x = document.getElementById("description1")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription2() {
+  let x = document.getElementById("description2")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription3() {
+  let x = document.getElementById("description3")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription4() {
+  let x = document.getElementById("description4")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription5() {
+  let x = document.getElementById("description5")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+// start row 2 left to right
+function showDescription6() {
+  let x = document.getElementById("description6")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription7() {
+  let x = document.getElementById("description7")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription8() {
+  let x = document.getElementById("description8")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription9() {
+  let x = document.getElementById("description9")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription10() {
+  let x = document.getElementById("description10")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+// start row 3 left to right
+function showDescription11() {
+  let x = document.getElementById("description11")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription12() {
+  let x = document.getElementById("description12")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription13() {
+  let x = document.getElementById("description13")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription14() {
+  let x = document.getElementById("description14")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription15() {
+  let x = document.getElementById("description15")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+// start row 4 left to right
+function showDescription16() {
+  let x = document.getElementById("description16")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription17() {
+  let x = document.getElementById("description17")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription18() {
+  let x = document.getElementById("description18")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription19() {
+  let x = document.getElementById("description19")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription20() {
+  let x = document.getElementById("description20")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+// start row 5 left to right
+function showDescription21() {
+  let x = document.getElementById("description21")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription22() {
+  let x = document.getElementById("description22")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription23() {
+  let x = document.getElementById("description23")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription24() {
+  let x = document.getElementById("description24")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
+
+function showDescription25() {
+  let x = document.getElementById("description25")
+    displayValue = "";
+  if (x.style.display == "")
+    displayValue = "none";
+
+  x.style.display = displayValue;
+}
